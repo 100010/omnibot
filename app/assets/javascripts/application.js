f14c0797ee8affbff20d30e6b4e67289d7830edf
100010 <yohei.senju@gmail.com>
2019-11-21 22:25:43 +0900

    Set up dashboard

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require core/jquery.min.js
+//= require core/bootstrap.min.js
+//= require core/popper.min.js
+//= require now-ui-dashboard.js
+//= require plugins/bootstrap-notify.js
+//= require plugins/chartjs.min.js
+//= require plugins/perfect-scrollbar.jquery.min.js
